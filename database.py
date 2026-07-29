fb4ca6cb9af2348022082f7e20a30f0a550386c9
hapticor <jangmiss9622@gmail.com>
2026-07-30 00:53:49 +0900

    Create database.py

---
+++ database.py
@@ -0,0 +1,25 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DB 주소 설정
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./chess.db")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# DB 세션 빌려주는 함수
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
